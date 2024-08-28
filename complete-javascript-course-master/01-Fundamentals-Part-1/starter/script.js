5372d7f8d8ee4b588cfcab4aa0219d882034f6f8
Njosephine <josephinenanyonga389@gmail.com>
2024-08-28 13:15:47 +0300

    first_commit

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,56 @@
+/*
+
+//Declare a variable js and assign it a value amazing
+let js = "amazing";
+// Check if the value of 'js' is equal to the string "amazing"
+if (js === "amazing") {
+// If the conditi// If the condition is true, display an alert message saying "JAVASCRIPT IS FUN"on is true, display an alert message saying "JAVASCRIPT IS FUN"
+ alert("JAVASCRIPT IS FUN");
+
+//the output for this computation doesnt   20 + 39 + 28; show output anywhere not even the console
+20 + 39 + 28;
+//for the output to be able to show use the console function
+//the console.log function builds abridge between the script and the developer side of the browser
+
+}
+
+console.log(20 + 39 + 28); 20 + 39 + 28;
+//declaring a variable and assigning it a value
+let firstName = 'matilda';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+//console.log(20 + 39 + 28); 20 + 39 + 28;
+
+//data types
+
+let javaFun = true;
+console.log(javaFun);
+console.log(typeof javaFun)
+
+console.log(typeof 23)
+console.log(typeof 'jose')
+
+javaFun = 'YES!';
+console.log(typeof javaFun);
+
+let year;
+console.log(typeof year)
+
+year = 1995;
+console.log(typeof year);*/
+
+
+//declaring variables using let,var andconsole.log(typeof year) const
+let age = 30;
+age = 31;
+
+const yearOfBirth = 1999;
+//console.log(typeof year)//yearOfBirth = 1993;// this returns an error it is not allowed
+
+var subject = 'biology';
+subject = 'physics';
+
+//we can also assign values to variables without first declaring them
+firstName = 'JOSEPHINE';// NOT ADVISABLE
+
